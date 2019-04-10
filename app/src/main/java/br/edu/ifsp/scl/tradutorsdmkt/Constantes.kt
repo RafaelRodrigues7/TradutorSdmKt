b7965dcb10364a19632b5f4bb0ac7f8533ed2fb6
Rafael <rafael_saocarlos@hotmail.com>
2019-04-10 19:33:01 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.tradutorsdmkt
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v1/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "2b580a49" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "21d6e833195cad33b393708f676889ef" // Preeencher com seu app_key
+}
